a86e1ca8b27a613a8aa1f560648a6034c96e23ba
knutaf <knutaf@gmail.com>
2016-12-03 23:49:48 -0800

    day 4 part 1

--- 04/part1.fsx
+++ 04/part1.fsx
@@ -11,6 +11,27 @@ let rexmatch pat str =
         List.tail [ for g in m.Groups -> g.Value ]
 ;;
 
+let compareDescending a b =
+    if a < b then
+        1
+    elif a > b then
+        -1
+    else 0
+;;
+
+let compareLetterCounts a b =
+    if (snd a) < (snd b) then
+        1
+    elif (snd a) > (snd b) then
+        -1
+    elif (fst a) > (fst b) then
+        1
+    elif (fst a) < (fst b) then
+        -1
+    else
+        0
+;;
+
 type room = { name : string; sector : int; checksum : string };;
 
 let parseRoom line =
@@ -20,6 +41,28 @@ let parseRoom line =
     | _ -> raise (Ex (sprintf "invalid room! %s" line))
 ;;
 
+let isRealRoom room =
+    let addLetter (letterMap : Map<char, int>) letter =
+        if letter <> '-' then
+            let newCount =
+                match Map.tryFind letter letterMap with
+                | None -> 1
+                | Some count -> count + 1
+            in
+            Map.add letter newCount letterMap
+        else
+            letterMap
+    in
+    let letterMap = Seq.fold addLetter Map.empty room.name in
+    let topLetters = List.map (fun (a, b) -> a) (List.sortWith compareLetterCounts (Map.toList letterMap)) in
+    let calculatedChecksum =
+        match topLetters with
+        | a :: b :: c :: d :: e :: rest -> Convert.ToString(a) + Convert.ToString(b) + Convert.ToString(c) + Convert.ToString(d) + Convert.ToString(e)
+        | _ -> raise (Ex (sprintf "invalid room doesn't have 5 top letters! %A" topLetters))
+    in
+    (*printfn "calc: %s, actual: %s" calculatedChecksum room.checksum;*)
+    calculatedChecksum = room.checksum
+;;
 
 let rec processLines sectorSum =
     let line = Console.ReadLine() in
@@ -27,8 +70,13 @@ let rec processLines sectorSum =
         sectorSum
     else
         let room = parseRoom line in
-        printfn "room: %A" room;
-        processLines sectorSum
+        let nextSectorSum =
+            if isRealRoom room then
+                sectorSum + room.sector
+            else
+                sectorSum
+        in
+        processLines nextSectorSum
 ;;
 
 printfn "sum of sector IDs = %d" (processLines 0)
