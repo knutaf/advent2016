f9b317a2048c09f8108c2a7b3f58aa58c8ecd11f
knutaf <knutaf@gmail.com>
2016-12-24 13:08:46 -0800

    day 11.1 - can carry to different floors now

--- 11/part1.fsx
+++ 11/part1.fsx
@@ -77,13 +77,13 @@ let string_from_slot slot =
     | Mic(e) -> (string_from_element e) + "M"
 ;;
 
-let drawGridWithCursor (floors : Set<Slot>[]) elevatorFloor numMoves r c selectedSlots =
+let drawGridWithCursor (floors : Set<Slot>[]) elevatorFloor numMoves r c selectedSlots errorMessage =
     Console.SetCursorPosition(0, 0);
     let printFloor floorNum =
         let floorSet = floors.[floorNum] in
         let printSlot slotNum slot =
             cprintf
-                (if floorNum = elevatorFloor && Set.contains slot selectedSlots then Some ConsoleColor.Red else None)
+                (if floorNum = elevatorFloor && Set.contains slot selectedSlots then Some ConsoleColor.Yellow else None)
                 (if floorNum = r && slotNum = c then Some ConsoleColor.DarkBlue else None)
                 "%2s "
                 (if Set.contains slot floorSet then (string_from_slot slot) else ".")
@@ -93,7 +93,20 @@ let drawGridWithCursor (floors : Set<Slot>[]) elevatorFloor numMoves r c selecte
         printfn ""
     in
     let _ = printfn "Moves: %d" numMoves in
-    for floorNum = ((Array.length floors) - 1) downto 0 do printFloor floorNum
+    for floorNum = ((Array.length floors) - 1) downto 0 do printFloor floorNum;
+    cprintf (Some ConsoleColor.Red) None "%s" errorMessage
+;;
+
+let isGen slot =
+    match slot with
+    | Gen _ -> true
+    | Mic _ -> false
+;;
+
+let isMic slot =
+    match slot with
+    | Gen _ -> false
+    | Mic _ -> true
 ;;
 
 let tryToggleSlotSelected (floors : Set<Slot>[]) elevatorFloor r c selectedSlots =
@@ -113,20 +126,69 @@ let tryToggleSlotSelected (floors : Set<Slot>[]) elevatorFloor r c selectedSlots
         selectedSlots
 ;;
 
-let tryPlacing (floors : Set<Slot>[]) elevatorFloor r c selectedSlots =
-    (floors, r, Set.empty<Slot>)
+let tryPlacing (floors : Set<Slot>[]) elevatorFloor r selectedSlots =
+    let hasPairedSlot slot floorSet =
+        match slot with
+        | Gen g -> Set.contains (Mic g) floorSet
+        | Mic m -> Set.contains (Gen m) floorSet
+    in
+    let getErrorMessageForUnshieldedMic exceptElement floorSet =
+        let checkForUnshieldedMic sofar slot =
+            match slot with
+            | Gen g -> sofar
+            | Mic m ->
+                if m = exceptElement then
+                    sofar
+                else
+                    if not (hasPairedSlot slot floorSet) then
+                        sprintf "%A missing pair" slot
+                    else
+                        sofar
+        in
+        Set.fold checkForUnshieldedMic "" floorSet
+    in
+    let getErrorMessageForFloorSafety floorSetForPlacing slotToPlace =
+        match slotToPlace with
+        | Gen g -> getErrorMessageForUnshieldedMic g floorSetForPlacing
+        | Mic m ->
+            if Set.contains (Gen m) floorSetForPlacing then
+                ""
+            elif Set.exists isGen floorSetForPlacing then
+                "floor has generators, and this mic is not shielded"
+            else
+                ""
+    in
+    let tryPlaceSlot (floorSet, errorMessage) slot =
+        if errorMessage = "" then
+            let errorForPlacing = getErrorMessageForFloorSafety floorSet slot in
+            if errorForPlacing = "" then
+                (Set.add slot floorSet, errorMessage)
+            else
+                (floorSet, errorForPlacing)
+        else
+            (floorSet, errorMessage)
+    in
+    let slotsList = Set.toList selectedSlots in
+    let (newFloorSet, errorMessage) = List.fold tryPlaceSlot (floors.[r], "") slotsList in
+    if errorMessage = "" then
+        let elevatorFloorSet = List.fold (fun floorSet slot -> Set.remove slot floorSet) floors.[elevatorFloor] slotsList in
+        let _ = floors.[elevatorFloor] <- elevatorFloorSet in
+        let _ = floors.[r] <- newFloorSet in
+        (floors, r, Set.empty<Slot>, errorMessage)
+    else
+        (floors, elevatorFloor, selectedSlots, errorMessage)
 ;;
 
-let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
+let rec processNextKey floors elevatorFloor numMoves r c selectedSlots errorMessage =
     let isCarrying = not (Set.isEmpty selectedSlots) in
     let moveCursor r c key =
         match key with
         | ConsoleKey.Spacebar ->
-            let (newFloors, newElevatorFloor, newSelectedSlots) =
+            let (newFloors, newElevatorFloor, newSelectedSlots, newErrorMessage) =
                 if r = elevatorFloor then
-                    (floors, elevatorFloor, tryToggleSlotSelected floors elevatorFloor r c selectedSlots)
+                    (floors, elevatorFloor, tryToggleSlotSelected floors elevatorFloor r c selectedSlots, "")
                 else
-                    tryPlacing floors elevatorFloor r c selectedSlots
+                    tryPlacing floors elevatorFloor r selectedSlots
             in
             (
             newFloors,
@@ -134,7 +196,8 @@ let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
             r,
             c,
             (if elevatorFloor <> newElevatorFloor then numMoves + 1 else numMoves),
-            newSelectedSlots
+            newSelectedSlots,
+            newErrorMessage
             )
         | ConsoleKey.RightArrow ->
             (
@@ -143,7 +206,8 @@ let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
             r,
             min (c + 1) ((Array.length ORDERED_SLOTS) - 1),
             numMoves,
-            selectedSlots
+            selectedSlots,
+            errorMessage
             )
         | ConsoleKey.LeftArrow ->
             (
@@ -152,7 +216,8 @@ let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
             r,
             max (c - 1) 0,
             numMoves,
-            selectedSlots
+            selectedSlots,
+            errorMessage
             )
         | ConsoleKey.UpArrow ->
             (
@@ -161,7 +226,8 @@ let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
             List.min [r + 1; elevatorFloor + (if isCarrying then 1 else 0); (Array.length INITIAL_FLOORS) - 1],
             c,
             numMoves,
-            selectedSlots
+            selectedSlots,
+            errorMessage
             )
         | ConsoleKey.DownArrow ->
             (
@@ -170,18 +236,19 @@ let rec processNextKey floors elevatorFloor numMoves r c selectedSlots =
             List.max [r - 1; elevatorFloor - (if isCarrying then 1 else 0); 0],
             c,
             numMoves,
-            selectedSlots
+            selectedSlots,
+            errorMessage
             )
-        | _ -> (floors, elevatorFloor, r, c, numMoves, selectedSlots)
+        | _ -> (floors, elevatorFloor, r, c, numMoves, selectedSlots, errorMessage)
     in
-    let _ = drawGridWithCursor floors elevatorFloor numMoves r c selectedSlots in
+    let _ = drawGridWithCursor floors elevatorFloor numMoves r c selectedSlots errorMessage in
     let keyInfo = Console.ReadKey(true) in
-    let (newFloors, newElevatorFloor, newR, newC, newNumMoves, newSelectedSlots) = moveCursor r c keyInfo.Key in
-    processNextKey newFloors newElevatorFloor newNumMoves newR newC newSelectedSlots
+    let (newFloors, newElevatorFloor, newR, newC, newNumMoves, newSelectedSlots, newErrorMessage) = moveCursor r c keyInfo.Key in
+    processNextKey newFloors newElevatorFloor newNumMoves newR newC newSelectedSlots newErrorMessage
 ;;
 
 Console.CursorVisible <- false;
 Console.Clear();
 
-processNextKey INITIAL_FLOORS 0 0 0 0 Set.empty<Slot>
+processNextKey INITIAL_FLOORS 0 0 0 0 Set.empty<Slot> ""
 ;;
