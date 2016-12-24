7a5ee6edf9a4b984ffee558cf69df28ec59e4115
knutaf <knutaf@gmail.com>
2016-12-23 23:13:32 -0800

    day 11.1 - select slots to carry

--- 11/part1.fsx
+++ 11/part1.fsx
@@ -1,6 +1,5 @@
 open System;;
 
-
 let cprintf fgColor bgColor fmt = 
     let printer (s : string) =
         let oldFG = System.Console.ForegroundColor in
@@ -38,6 +37,32 @@ type Slot =
 | Mic of Element
 ;;
 
+let MAX_CARRY = 2;;
+
+let ORDERED_SLOTS =
+    [|
+        Gen P;
+        Mic P;
+        Gen C;
+        Mic C;
+        Gen B;
+        Mic B;
+        Gen R;
+        Mic R;
+        Gen F;
+        Mic F;
+    |]
+;;
+
+let INITIAL_FLOORS =
+    [|
+        Set.ofList [ Gen P; Mic P ];
+        Set.ofList [ Gen C; Gen B; Gen R; Gen F ];
+        Set.ofList [ Mic C; Mic B; Mic R; Mic F ];
+        Set.empty<Slot>
+    |]
+;;
+
 let string_from_slot slot =
     let string_from_element element =
         match element with
@@ -52,64 +77,84 @@ let string_from_slot slot =
     | Mic(e) -> (string_from_element e) + "M"
 ;;
 
-let ORDERED_SLOTS =
-    [
-        Gen P;
-        Mic P;
-        Gen C;
-        Mic C;
-        Gen B;
-        Mic B;
-        Gen R;
-        Mic R;
-        Gen F;
-        Mic F;
-    ]
-;;
-
-let drawGridWithCursor (floors : Set<Slot>[]) elevatorFloor r c =
+let drawGridWithCursor (floors : Set<Slot>[]) elevatorFloor r c selectedSlots =
     Console.SetCursorPosition(0, 0);
     let printFloor floorNum =
-        let fl = floors.[floorNum] in
+        let floorSet = floors.[floorNum] in
         let printSlot slotNum slot =
             cprintf
-                None
+                (if floorNum = elevatorFloor && Set.contains slot selectedSlots then Some ConsoleColor.Red else None)
                 (if floorNum = r && slotNum = c then Some ConsoleColor.DarkBlue else None)
                 "%2s "
-                (if Set.contains slot fl then (string_from_slot slot) else ".")
+                (if Set.contains slot floorSet then (string_from_slot slot) else ".")
         in
         let _ = printf "F%d %s  " (floorNum + 1) (if elevatorFloor = floorNum then "E" else ".") in
-        let _ = List.iteri printSlot ORDERED_SLOTS in
+        let _ = Array.iteri printSlot ORDERED_SLOTS in
         printfn ""
     in
     for floorNum = ((Array.length floors) - 1) downto 0 do printFloor floorNum
 ;;
 
-let rec loop floors elevatorFloor r c =
+let tryToggleSlotSelected (floors : Set<Slot>[]) elevatorFloor r c selectedSlots =
+    if r = elevatorFloor then
+        let slotUnderCursor = ORDERED_SLOTS.[c] in
+        let floorSet = floors.[r] in
+        if Set.contains slotUnderCursor floorSet then
+            if Set.contains slotUnderCursor selectedSlots then
+                Set.remove slotUnderCursor selectedSlots
+            elif (Set.count selectedSlots) < MAX_CARRY then
+                Set.add slotUnderCursor selectedSlots
+            else
+                selectedSlots
+        else
+            selectedSlots
+    else
+        selectedSlots
+;;
+
+let rec processNextKey floors elevatorFloor r c selectedSlots =
     let moveCursor r c key =
         match key with
-        | ConsoleKey.RightArrow -> (r, c + 1)
-        | ConsoleKey.LeftArrow -> (r, c - 1)
-        | ConsoleKey.UpArrow -> (r - 1, c)
-        | ConsoleKey.DownArrow -> (r + 1, c)
-        | _ -> (r, c)
+        | ConsoleKey.Spacebar ->
+            (
+            r,
+            c,
+            tryToggleSlotSelected floors elevatorFloor r c selectedSlots
+            )
+        | ConsoleKey.RightArrow ->
+            (
+            r,
+            min (c + 1) ((Array.length ORDERED_SLOTS) - 1),
+            selectedSlots
+            )
+        | ConsoleKey.LeftArrow ->
+            (
+            r,
+            max (c - 1) 0,
+            selectedSlots
+            )
+        | ConsoleKey.UpArrow ->
+            (
+            min (r + 1) ((Array.length INITIAL_FLOORS) - 1),
+            c,
+            selectedSlots
+            )
+        | ConsoleKey.DownArrow ->
+            (
+            max (r - 1) 0,
+            c,
+            selectedSlots
+            )
+        | _ -> (r, c, selectedSlots)
     in
-    let _ = drawGridWithCursor floors elevatorFloor r c in
+    let _ = drawGridWithCursor floors elevatorFloor r c selectedSlots in
     let keyInfo = Console.ReadKey(true) in
-    let (newR, newC) = moveCursor r c keyInfo.Key in
-    loop floors elevatorFloor newR newC
+    let (newR, newC, newSelectedSlots) = moveCursor r c keyInfo.Key in
+    processNextKey floors elevatorFloor newR newC newSelectedSlots
 ;;
 
 Console.CursorVisible <- false;
 Console.Clear();
 
-let initialFloors =
-    [|
-        Set.ofList [ Gen P; Mic P ];
-        Set.ofList [ Gen C; Gen B; Gen R; Gen F ];
-        Set.ofList [ Mic C; Mic B; Mic R; Mic F ];
-        Set.empty<Slot>
-    |]
-in
-loop initialFloors 0 0 0
+processNextKey INITIAL_FLOORS 0 0 0 Set.empty<Slot>
 ;;
