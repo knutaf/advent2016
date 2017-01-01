5b169b0a90a21b7432b79f2079edd87adf7620c3
knutaf <knutaf@gmail.com>
2017-01-01 00:05:53 -0800

    day 12 parts 1 and 2

--- 12/part1.fsx
+++ 12/part1.fsx
@@ -8,7 +8,8 @@ type Instruction =
     | CopyRegister of string * string
     | Inc of string
     | Dec of string
-    | JumpNotZero of string * int
+    | JumpNotZeroImmediate of int * int
+    | JumpNotZeroReg of string * int
 ;;
 
 type State = { inst : int; registers : Map<string, int> };;
@@ -34,8 +35,12 @@ let parseInstruction line =
                 match rexmatch @"^dec ([a-zA-Z]+)$" line with
                 | [a] -> Dec(a)
                 | [] ->
-                    match rexmatch @"^jnz ([a-zA-Z]+) (-?\d+)$" line with
-                    | [a; b] -> JumpNotZero(a, Convert.ToInt32(b))
+                    match rexmatch @"^jnz (-?\d+) (-?\d+)$" line with
+                    | [a; b] -> JumpNotZeroImmediate(Convert.ToInt32(a), Convert.ToInt32(b))
+                    | [] ->
+                        match rexmatch @"^jnz ([a-zA-Z]+) (-?\d+)$" line with
+                        | [a; b] -> JumpNotZeroReg(a, Convert.ToInt32(b))
+                        | _ -> raise (Ex (sprintf "invalid directive! %s" line))
                     | _ -> raise (Ex (sprintf "invalid directive! %s" line))
                 | _ -> raise (Ex (sprintf "invalid directive! %s" line))
             | _ -> raise (Ex (sprintf "invalid directive! %s" line))
@@ -53,12 +58,28 @@ let rec processLines programSoFar =
 ;;
 
 let rec runProgram program state =
+    (*let _ = printfn "state: %A" state in*)
+    let doJumpNotZero value offset =
+        let instOffset =
+            if value = 0 then
+                1
+            else
+                offset
+        in
+        { inst = state.inst + instOffset; registers = state.registers }
+    in
     if state.inst < (Array.length program) then
+        (*let _ = printfn "inst: %A" program.[state.inst] in*)
         let newState =
             match program.[state.inst] with
             | CopyImmediate(value, reg) -> { inst = state.inst + 1; registers = (Map.add reg value state.registers) }
-            | _ -> { inst = state.inst + 1; registers = state.registers }
+            | CopyRegister(srcReg, destReg) -> { inst = state.inst + 1; registers = (Map.add destReg (Map.tryFind srcReg state.registers).Value state.registers) }
+            | Inc(reg) -> { inst = state.inst + 1; registers = (Map.add reg ((Map.tryFind reg state.registers).Value + 1) state.registers) }
+            | Dec(reg) -> { inst = state.inst + 1; registers = (Map.add reg ((Map.tryFind reg state.registers).Value - 1) state.registers) }
+            | JumpNotZeroImmediate(value, offset) -> doJumpNotZero value offset
+            | JumpNotZeroReg(reg, offset) -> doJumpNotZero (Map.tryFind reg state.registers).Value offset
         in
+        (*let _ = printfn "" in*)
         runProgram program newState
     else
         state
@@ -66,17 +87,18 @@ let rec runProgram program state =
 
 let REGISTERS =
     [
-        "a";
-        "b";
-        "c";
-        "d";
+        ("a", 0);
+        ("b", 0);
+        ("c", 1);
+        ("d", 0);
     ]
 ;;
 
 let instructionList = processLines [] in
 let program = Array.ofList instructionList in
-let _ = List.iter (printfn "%A") instructionList in
-let initialRegisters = List.fold (fun map reg -> Map.add reg 0 map) Map.empty REGISTERS in
+let _ = List.iteri (printfn "%3d: %A") instructionList in
+let _ = printfn "" in
+let initialRegisters = List.fold (fun map (reg, value) -> Map.add reg value map) Map.empty REGISTERS in
 let finalState = runProgram program { inst = 0; registers = initialRegisters } in
 printfn "%A" finalState
 ;;
