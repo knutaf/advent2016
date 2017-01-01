d293736186b9580928c009eb111f426548b2427a
knutaf <knutaf@gmail.com>
2017-01-01 01:23:27 -0800

    day 13.1 - solves test input

--- 13/part1.fsx
+++ 13/part1.fsx
@@ -1,5 +1,7 @@
 open System;;
 
+exception Ex of string;;
+
 let countBits value =
     let incIfBit sofar bitPos =
         if (value &&& (1UL <<< bitPos)) = 0UL then
@@ -10,17 +12,17 @@ let countBits value =
     Seq.fold incIfBit 0 (seq { 0 .. 63 })
 ;;
 
-let isOpen favNumber (x, y) =
+let isOpen favoriteNumber (x, y) =
     let ui64X = uint64 x in
     let ui64Y = uint64 y in
-    let calc = (ui64X * ui64X) + (3UL * ui64X) + (2UL * ui64X * ui64Y) + ui64Y + (ui64Y * ui64Y) + (uint64 favNumber) in
+    let calc = (ui64X * ui64X) + (3UL * ui64X) + (2UL * ui64X * ui64Y) + ui64Y + (ui64Y * ui64Y) + (uint64 favoriteNumber) in
     ((countBits calc) % 2) = 0
 ;;
 
-let printGrid favNumber sizeX sizeY =
+let printGrid favoriteNumber sizeX sizeY =
     let printRow y =
         printf "%-2d " y;
-        for x = 0 to (sizeX - 1) do printf "%2s " (if (isOpen favNumber (x, y)) then "." else "#");
+        for x = 0 to (sizeX - 1) do printf "%2s " (if (isOpen favoriteNumber (x, y)) then "." else "#");
         printfn ""
     in
     printf "   ";
@@ -30,6 +32,46 @@ let printGrid favNumber sizeX sizeY =
     printfn "";
 ;;
 
+type State = { pos : int * int; numMoves : int };;
+type HistoryEntry = { state : State; parent : HistoryEntry option };;
+
+let expandEntry favoriteNumber history frontier entry =
+    let moveOffsets = [(-1, 0); (0, -1); (1, 0); (0, 1)] in
+    let addOffsetIfValid sofar offset =
+        let newPos = ((fst entry.state.pos) + (fst offset), ((snd entry.state.pos) + (snd offset))) in
+        if (fst newPos) >= 0 && (snd newPos) >= 0 && (isOpen favoriteNumber newPos) then
+            if not (Set.contains newPos history) then
+                if (List.tryFind (fun elem -> elem.state.pos = newPos) frontier).IsNone then
+                    { state = { pos = newPos; numMoves = entry.state.numMoves + 1 }; parent = Some entry } :: sofar
+                else
+                    sofar
+            else
+                sofar
+        else
+            sofar
+    in
+    List.fold addOffsetIfValid [] moveOffsets
+;;
+
+let rec bfs favoriteNumber targetPosition iterations history frontier =
+    let _ =
+        if (iterations % 1) = 0 then
+            printfn "historySize %d, frontierSize %d, numMoves here %d" (Set.count history) (List.length frontier) (List.head frontier).state.numMoves
+        else
+            ()
+    in
+    match frontier with
+    | [] ->
+        printfn "history: %A" history;
+        raise (Ex "empty frontier!")
+    | nextEntryToExpand :: restOfFrontier ->
+        let expandedEntries = expandEntry favoriteNumber history frontier nextEntryToExpand in
+        let maybeFinalEntry = List.tryFind (fun elem -> elem.state.pos = targetPosition) expandedEntries in
+        match maybeFinalEntry with
+        | Some finalEntry -> finalEntry
+        | None -> bfs favoriteNumber targetPosition (iterations + 1) (Set.add nextEntryToExpand.state.pos history) (restOfFrontier @ expandedEntries)
+;;
+
 [<EntryPoint>]
 let main argv =
     match argv with
@@ -37,8 +79,8 @@ let main argv =
         let favoriteNumber = Convert.ToInt32(favoriteNumberStr) in
         let destX = Convert.ToInt32(destXStr) in
         let destY = Convert.ToInt32(destYStr) in
-        let _ = printGrid favoriteNumber 10 10 in
-        printfn "%d %d %d" favoriteNumber destX destY
+        let finalEntry = bfs favoriteNumber (destX, destY) 0 Set.empty [{ state = { pos = (1, 1); numMoves = 0 }; parent = None }] in
+        printfn "numMoves: %d" finalEntry.state.numMoves
     | _ -> printfn "need favorite number, dest x, and dest y"
     0
 ;;
