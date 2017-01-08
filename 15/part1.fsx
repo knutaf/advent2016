7621d6a0c3d21817c95bfdaf109fdd7d8537023d
knutaf <knutaf@gmail.com>
2017-01-07 23:50:33 -0800

    day 15 part 1

--- 15/part1.fsx
+++ 15/part1.fsx
@@ -3,7 +3,7 @@ open System.Text.RegularExpressions;;
 
 exception Ex of string;;
 
-type Disc = { num : int; numPositions : int; startingPosition : int };;
+type Disc = { num : int; numPositions : int; currentPosition : int };;
 
 let rexmatch pat str =
     let m = Regex.Match(str, pat) in
@@ -15,7 +15,9 @@ let rexmatch pat str =
 
 let parseDisc line =
     match rexmatch @"^Disc \#(\d+) has (\d+) positions; at time=0, it is at position (\d+)\.$" line with
-    | [numStr; numPositionsStr; startingPositionStr] -> { num = Convert.ToInt32(numStr); numPositions = Convert.ToInt32(numPositionsStr); startingPosition = Convert.ToInt32(startingPositionStr) }
+    | [numStr; numPositionsStr; startingPositionStr] ->
+        let numPositions = Convert.ToInt32(numPositionsStr) in
+        { num = Convert.ToInt32(numStr); numPositions = numPositions; currentPosition = ((Convert.ToInt32(startingPositionStr)) + 1) % numPositions }
     | _ -> raise (Ex (sprintf "invalid disc! %s" line))
 ;;
 
@@ -28,6 +30,31 @@ let rec loadDiscs discsSoFar =
         loadDiscs (discsSoFar @ [disc])
 ;;
 
+let advanceDiscs discs =
+    List.map (fun disc -> { disc with currentPosition = ((disc.currentPosition + 1) % disc.numPositions) }) discs
+;;
+
+let rec doesFallThrough discs =
+    match discs with
+    | [] -> true
+    | disc :: restOfDiscs ->
+        if disc.currentPosition = 0 then
+            doesFallThrough (advanceDiscs restOfDiscs)
+        else
+            false
+;;
+
+let findFirstGoodStartingTime discs =
+    let rec helper discs startingTime =
+        if doesFallThrough discs then
+            startingTime
+        else
+            helper (advanceDiscs discs) (startingTime + 1)
+    in
+    helper discs 0
+;;
+
 let discs = loadDiscs [] in
-printfn "%A" discs
+let goodStartingTime = findFirstGoodStartingTime discs in
+printfn "ball fell through at time=%d" goodStartingTime
 ;;
