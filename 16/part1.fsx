123e0730d54ad7f6df3eee916603c1e286b1ea2e
knutaf <knutaf@gmail.com>
2017-01-09 22:25:41 -0800

    16.1 - munges correctly to required disk size

--- 16/part1.fsx
+++ 16/part1.fsx
@@ -16,13 +16,28 @@ let dataToString data =
     List.fold (fun sofar d -> sofar + (if d then "1" else "0")) "" data
 ;;
 
+
+let munge a =
+    let b = List.rev a in
+    let bInverse = List.map (not) b in
+    a @ (false::bInverse)
+;;
+
+let rec mungeUntilSize data size =
+    if List.length data >= size then
+        data
+    else
+        mungeUntilSize (munge data) size
+;;
+
 [<EntryPoint>]
 let main argv =
     match argv with
     | [| dataStr; diskSizeStr |] ->
         let inputData = parseInput dataStr in
         let diskSize = Convert.ToInt32(diskSizeStr) in
-        printfn "input data: %s, size: %d" (dataToString inputData) diskSize
+        let _ = printfn "input data: %s, size: %d" (dataToString inputData) diskSize in
+        printfn "munged: %s" (dataToString (mungeUntilSize inputData diskSize))
     | _ -> printfn "need data string and disk size"
     0
 ;;
