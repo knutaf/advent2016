8d85d04a51c3c80c2ae8b0fe0a453d54af37691b
knutaf <knutaf@gmail.com>
2017-01-09 22:47:48 -0800

    day 16 part 1

--- 16/part1.fsx
+++ 16/part1.fsx
@@ -41,6 +41,20 @@ let rec mungeUntilSize data size =
         mungeUntilSize (munge data) size
 ;;
 
+let rec calculateChecksum data =
+    let rec helper checksumSoFar data =
+        match data with
+        | [] -> checksumSoFar
+        | a :: b :: rest -> helper (checksumSoFar @ [a = b]) rest
+        | _ -> raise (Ex (sprintf "how did this happen!? %A" data))
+    in
+    let checksumStep = helper [] data in
+    if ((List.length checksumStep) % 2 = 1) then
+        checksumStep
+    else
+        calculateChecksum checksumStep
+;;
+
 [<EntryPoint>]
 let main argv =
     match argv with
@@ -48,7 +62,12 @@ let main argv =
         let inputData = parseInput dataStr in
         let diskSize = Convert.ToInt32(diskSizeStr) in
         let _ = printfn "input data: %s, size: %d" (dataToString inputData) diskSize in
-        printfn "munged: %s" (dataToString (mungeUntilSize inputData diskSize))
+        if (diskSize % 2) = 0 then
+            let mungedData = mungeUntilSize inputData diskSize in
+            let _ = printfn "munged: %s" (dataToString mungedData) in
+            printfn "checksum: %s" (dataToString (calculateChecksum mungedData))
+        else
+            printfn "can't calculate checksum of odd size!"
     | _ -> printfn "need data string and disk size"
     0
 ;;
