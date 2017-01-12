3459bd84d32a10ce6bf1188d00e9946f8ab0311b
knutaf <knutaf@gmail.com>
2017-01-11 22:30:06 -0800

    17.1 - finds first path, but not shortest

--- 17/part1.fsx
+++ 17/part1.fsx
@@ -16,6 +16,8 @@ let g_md5 = MD5.Create("MD5");;
 
 let GRID_SIZE_X = 4;;
 let GRID_SIZE_Y = 4;;
+let START = (0, 0);;
+let TARGET = (3, 3);;
 
 type Direction =
     | Up
@@ -24,12 +26,12 @@ type Direction =
     | Right
 ;;
 
-let getOpenDoors x y pathStr =
+let getOpenDoors (x, y) pathStr =
     let isHashNibbleOpen nibble =
         nibble > 0xauy
     in
     let md5Bytes = g_md5.ComputeHash(toByteArray pathStr) in
-    let _ = printfn "hash: %s" (toHexString md5Bytes) in
+    //let _ = printfn "hash: %s" (toHexString md5Bytes) in
     let ls0 = [] in
     let ls1 =
         if y > 0 then
@@ -41,18 +43,18 @@ let getOpenDoors x y pathStr =
             ls0
     in
     let ls2 =
-        if y < GRID_SIZE_Y - 1 then
-            if isHashNibbleOpen (md5Bytes.[0] &&& 0x0fuy) then
-                Down :: ls1
+        if x > 0 then
+            if isHashNibbleOpen ((md5Bytes.[1] &&& 0xf0uy) >>> 4) then
+                Left :: ls1
             else
                 ls1
         else
             ls1
     in
     let ls3 =
-        if x > 0 then
-            if isHashNibbleOpen ((md5Bytes.[1] &&& 0xf0uy) >>> 4) then
-                Left :: ls2
+        if y < GRID_SIZE_Y - 1 then
+            if isHashNibbleOpen (md5Bytes.[0] &&& 0x0fuy) then
+                Down :: ls2
             else
                 ls2
         else
@@ -70,11 +72,67 @@ let getOpenDoors x y pathStr =
     ls4
 ;;
 
+let movePos (x, y) dir =
+    match dir with
+    | Up -> (x, y - 1)
+    | Down -> (x, y + 1)
+    | Left -> (x - 1, y)
+    | Right -> (x + 1, y)
+;;
+
+let stringFromDir dir =
+    match dir with
+    | Up -> "U"
+    | Down -> "D"
+    | Left -> "L"
+    | Right -> "R"
+;;
+
+let dirFromChar ch =
+    match ch with
+    | 'U' -> Up
+    | 'D' -> Down
+    | 'L' -> Left
+    | 'R' -> Right
+    | _ -> raise (Ex (sprintf "invalid dir char! %c" ch))
+;;
+
+let verifyPath pathStr =
+    let endPos =
+        Seq.fold (fun pos ch ->
+            let nextPos = movePos pos (dirFromChar ch) in
+            let _ = printfn "%A -> %c -> %A" pos ch nextPos in
+            nextPos
+            ) START pathStr
+    in
+    printfn "ended at: %A" endPos
+;;
+
+// TODO: find the shortest path, not the first successful path
+let dfs passcode =
+    let rec helper pos pathStr =
+        if pos = TARGET then
+            Some pathStr
+        else
+            let openDoors = getOpenDoors pos pathStr in
+            List.fold (fun anyPath dir ->
+                if anyPath.IsSome then
+                    anyPath
+                else
+                    helper (movePos pos dir) (pathStr + (stringFromDir dir))
+                ) None openDoors
+    in
+    helper START passcode
+;;
+
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| passcode; path |] ->
-        printfn "dirs: %A" (getOpenDoors 2 2 (passcode + path))
+    | [| passcode |] ->
+        let pathWithPasscode = (dfs passcode).Value in
+        let path = pathWithPasscode.Substring(String.length passcode) in
+        let _ = verifyPath path in
+        printfn "path: %s" path
     | _ -> printfn "need passcode"
     0
 ;;
