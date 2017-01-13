0742caba926f1f0e55bdb2415e8d3851a216b67b
knutaf <knutaf@gmail.com>
2017-01-12 22:50:44 -0800

    19.1 - can run a single round at a time

--- 19/part1.fsx
+++ 19/part1.fsx
@@ -5,9 +5,41 @@ exception Ex of string;;
 
 type Elf = { id: int; presents: int };;
 
-let stealUntilDone numElves =
-    let initialElves = List.ofSeq (seq { for id in 1 .. numElves -> { id = id; presents = 1 } }) in
-    let _ = printfn "len: %d" (List.length initialElves) in
+let stealUntilDone startingNumElves =
+    let runRound elves numElves =
+        let _ = assert(numElves > 1) in
+        let isOddNumber = (numElves % 2) = 1 in
+        let elvesStartingRound =
+            if isOddNumber then
+                List.tail elves
+            else
+                elves
+        in
+        let numPresentsForEach = (startingNumElves / (numElves / 2)) in
+        List.foldBack (fun elf (nextElvesSoFar, nextElvesSoFarLength, numTraversed) ->
+            if (not isOddNumber) && (numTraversed = 0) then
+                (nextElvesSoFar, nextElvesSoFarLength, numTraversed + 1)
+            else
+                if ((numTraversed % 2) = 0) = isOddNumber then
+                    let newElf =
+                        {
+                            id = elf.id;
+                            presents =
+                                numPresentsForEach +
+                                if isOddNumber && (numTraversed = 0) then
+                                    1
+                                else
+                                    0
+                        }
+                    in
+                    (newElf :: nextElvesSoFar, nextElvesSoFarLength + 1, numTraversed + 1)
+                else
+                    (nextElvesSoFar, nextElvesSoFarLength, numTraversed + 1)
+            ) elvesStartingRound ([], 0, 0)
+    in
+    let initialElves = List.ofSeq (seq { for id in 1 .. startingNumElves -> { id = id; presents = 1 } }) in
+    let (nextElves, nextElvesLength, numTraversed) = runRound initialElves startingNumElves in
+    let _ = printfn "after round: %A" (List.map (fun elf -> (elf.id, elf.presents)) nextElves) in
     List.head initialElves
 ;;
 
